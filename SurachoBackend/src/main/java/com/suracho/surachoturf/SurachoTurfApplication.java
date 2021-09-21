7ae30e7bdbd0c01bf2f361042df99915289eadb9
Suracho <suraj-rajesh.chodankar@capgemini.com>
2021-09-21 12:09:07 +0530

    first commit backend

---
+++ SurachoBackend/src/main/java/com/suracho/surachoturf/SurachoTurfApplication.java
@@ -0,0 +1,13 @@
+package com.suracho.surachoturf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurachoTurfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SurachoTurfApplication.class, args);
+    }
+
+}
---
+++ SurachoBackend/src/test/java/com/suracho/surachoturf/SurachoTurfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suracho.surachoturf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurachoTurfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
